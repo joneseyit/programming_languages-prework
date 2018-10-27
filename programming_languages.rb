c6497e5210a17ee184e3786927af6e58956be961
joneseyit <joneseyit@protonmail.com>
2018-10-27 00:48:14 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,8 @@ def reformat_languages(languages)
   languages.each do |style, target_lang|
 
     target_lang.each do |language, types|
-
+        if xyz.has_key != nil
+          
         new_hash[language] = {type: types[:type], style: [style]}
         #new_hash[language][:style] =  style
 
